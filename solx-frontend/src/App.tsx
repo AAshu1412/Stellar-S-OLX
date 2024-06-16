a2ec4e6cc6e5f5ceef95aba72f883e6b4761e370
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-06-16 20:12:55 +0530

    finally chal gya

--- solx-frontend/src/App.tsx
+++ solx-frontend/src/App.tsx
@@ -1,10 +1,42 @@
 import { useState } from 'react'
 import helloWorld from "./contracts/hello_world";
+import {
+    isConnected,
+    getPublicKey,
+    signAuthEntry,
+    signTransaction,
+    signBlob,requestAccess
+  } from "@stellar/freighter-api";
 
 function App() {
+    
 
     (async()=>{
-        const { result } = await helloWorld.initialize({name: "POPPO", address: "GBZV3NONYSUDVTEHATQO4BCJVFXJO3XQU5K32X3XREVZKSMMOZFO4ZXR"});
+        if (await isConnected()) {
+            alert("User has Freighter!");
+          }
+
+          
+          const retrievePublicKey = async () => {
+            let publicKey = "";
+            let error = "";
+          
+            try {
+              publicKey = await requestAccess();
+            } catch (e) {
+              console.log(e);
+            }
+          
+            if (error) {
+              return error;
+            }
+          
+            return publicKey;
+          };
+          
+          const res = retrievePublicKey();
+          console.log(res);
+        const { result } = await helloWorld.balance({id: "GBZV3NONYSUDVTEHATQO4BCJVFXJO3XQU5K32X3XREVZKSMMOZFO4ZXR"});
         console.log(result);
 console.log("dad");
     })();
